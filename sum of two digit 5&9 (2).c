4886ad5062961057b558d3786c469b5d3603adad
Sonam Agrawal <64350434+sonam-ag@users.noreply.github.com>
2023-08-02 17:01:09 +0530

    Update sum of two digit 5&9 (2).c

--- sum of two digit 5&9 (2).c
+++ sum of two digit 5&9 (2).c
@@ -2,10 +2,10 @@
 #include<conio.h>
 int main()
 {
-int a,b,sum;
-a=5;
-b=9;
-sum = a + b;
-printf("%d",sum);
-getch();
+  int a,b,sum;
+  a=5;
+  b=9;
+  sum = a + b;
+  printf("%d",sum);
+  getch();
 }
