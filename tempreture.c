dc35758848f90d6593f2c330031d4393cd613218
Sonam Agrawal <64350434+sonam-ag@users.noreply.github.com>
2023-08-03 19:00:34 +0530

    Update tempreture.c

--- tempreture.c
+++ tempreture.c
@@ -2,10 +2,10 @@
 #include<conio.h>
 int main()
 {
-float fahrenhenit ,centigrade;
-printf("enter tempreture in fahrenhenit:");
-scanf("%f",&fahrenhenit);
-centigrade = (fahrenhenit-32)*5/9;
-printf("centigrade = %f",centigrade);
-getch();
+  float fahrenhenit ,centigrade;
+  printf("enter tempreture in fahrenhenit:");
+  scanf("%f",&fahrenhenit);
+  centigrade = (fahrenhenit-32)*5/9;
+  printf("centigrade = %f",centigrade);
+  getch();
 }
