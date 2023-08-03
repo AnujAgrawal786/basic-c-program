7fae63f2357ff60deac61819f24c4ffb8ca1cd4a
AnujAgrawal786 <88234650+AnujAgrawal786@users.noreply.github.com>
2023-08-03 18:54:12 +0530

    Add files via upload

---
+++ tempreture.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+float fahrenhenit ,centigrade;
+printf("enter tempreture in fahrenhenit:");
+scanf("%f",&fahrenhenit);
+centigrade = (fahrenhenit-32)*5/9;
+printf("centigrade = %f",centigrade);
+getch();
+}
