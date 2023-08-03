267315e99ee8fae17cba331558abf816a3be5994
Sonam Agrawal <64350434+sonam-ag@users.noreply.github.com>
2023-08-03 18:59:08 +0530

    Update area.c

--- area.c
+++ area.c
@@ -2,17 +2,17 @@
 #include <conio.h>
 int main()
 {
-float length , width , radius ;
-float arearectangle , areacircle ;
-printf("enter rectangle length:");
-scanf("%f",&length);
-printf("enter rectangle width:");
-scanf("%f",&width);
-arearectangle = length * width ;
-printf("area of rectangle = %f",arearectangle);
-printf("\nenter circle radius:");
-scanf("%f",&radius);
-areacircle = 3.14 * radius * radius;
-printf("%f",areacircle);
-getch();
+  float length , width , radius ;
+  float arearectangle , areacircle ;
+  printf("enter rectangle length:");
+  scanf("%f",&length);
+  printf("enter rectangle width:");
+  scanf("%f",&width);
+  arearectangle = length * width ;
+  printf("area of rectangle = %f",arearectangle);
+  printf("\nenter circle radius:");
+  scanf("%f",&radius);
+  areacircle = 3.14 * radius * radius;
+  printf("%f",areacircle);
+  getch();
 }
